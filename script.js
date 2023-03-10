fab3addc9d398df12c3c8d7e7b872c12069092e0
reginaldomig <reginaldomig@gmail.com>
2023-03-09 21:58:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ var vagas = 10
 
 
 function estacionar(){
+    if(vagas <0){
+        console.log("As vagas acabaram")
+    }else{
+        console.log("Estaciona o Carro")
+        vagas = vagas -1
+    }
     
 }
