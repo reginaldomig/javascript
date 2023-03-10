afcd6de987132c1f2bebb61e0f688be20d94b9ff
ReginaldoMiguel <reginaldomig@gmail.com>
2023-03-09 22:55:33 -0300

    e

---
+++ exercicios/estruturaCondicional.js
@@ -0,0 +1,7 @@
+var luzLigada = true
+
+if(luzLigada == true){
+    console.log('A Luz está Ligada. Desligue após sair')
+}else{
+    console.log('Ligue a Luz, mas deslique depois de sair')
+}
--- exercicios/variaveis.js
+++ exercicios/variaveis.js
@@ -4,3 +4,4 @@ var anoAtual = 2023
 
 // Em que ano Josefina nasceu?
 
+console.log('Coloque aqui o resultado')
